91ed2e282a0242bbcdf637b1403e6ea6809608cd
Sergey <piatrovi@sheridancollege.ca>
2019-01-22 21:10:25 -0500

    Accept different lenght parameters

--- calculator.py
+++ calculator.py
@@ -3,8 +3,5 @@ calculator
 """
 
 def add(*args):
-    """ calculator """
-    answer = 0
-    for value in args:
-        answer += value
-    return answer
+    """ Return sum or n-parameters  """
+    return sum(args)
