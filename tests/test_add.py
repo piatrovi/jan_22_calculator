2ff2a9fe5f9142a5a0834d83c1f96f5e44fecd41
Sergey <piatrovi@sheridancollege.ca>
2019-01-22 20:51:41 -0500

    add  test_three_and_three()

--- tests/test_add.py
+++ tests/test_add.py
@@ -5,5 +5,9 @@ Test add() function of the calculator
 from calculator import add
 
 def test_two_plus_two():
-    """ If given 2 and 2 as parameters, should be returned"""
+    """ If given 2 and 2 as parameters, 4 should be returned"""
     assert add(2, 2) == 4
+
+def test_three_and_three():
+    """ If given 3 and 3 as parameters, 4 should be returned"""
+    assert add(3, 3) == 6
